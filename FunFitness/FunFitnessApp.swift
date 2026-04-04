81743637253c4671bad4aa2e38c73d4536de00fb
Jay Jones <jay@jaytjones.com>
2026-04-04 14:21:16 -0500

    Addressed this suggestion: Custom ModelContainer Initialization ⚠️ Overly Complex

--- FunFitness/FunFitnessApp.swift
+++ FunFitness/FunFitnessApp.swift
@@ -1,4 +1,4 @@
-//
+    //
 //  FunFitnessApp.swift
 //  FunFitness
 //
@@ -10,20 +10,21 @@ import SwiftData
 
 @main
 struct FunFitnessApp: App {
-    var sharedModelContainer: ModelContainer = {
+    var sharedModelContainer: ModelContainer {
         let schema = Schema([
             UserProfile.self,
             ActivityLog.self,
             UnlockedAchievement.self,
         ])
+        
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
+        
         do {
             return try ModelContainer(for: schema, configurations: [modelConfiguration])
         } catch {
             fatalError("Could not create ModelContainer: \(error)")
         }
-    }()
+    }
 
     var body: some Scene {
         WindowGroup {
